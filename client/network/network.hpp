57f168f32f3c5e421a0d580c4405ad94a7862782
xifil <iamspaceyt@gmail.com>
2025-03-07 22:06:10 +0000

    fix an inline

--- client/network/network.hpp
+++ client/network/network.hpp
@@ -6,7 +6,7 @@ namespace Client::Network {
 	size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
 	std::optional<std::string> GetContent(const std::string& url);
 
-	std::optional<nlohmann::json> GetContentAsJson(const std::string& url) {
+	inline std::optional<nlohmann::json> GetContentAsJson(const std::string& url) {
 		std::optional<std::string> content = GetContent(url);
 		return content.has_value() ? std::optional{ nlohmann::json::parse(content.value()) } : std::nullopt;
 	}
